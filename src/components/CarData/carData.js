8cfd85edabe88fe8dc31ed5e9935d641ae27f024
Rachit Narula <narularachit22@gmail.com>
2023-10-04 23:44:35 +0530

    id

--- src/components/CarData/carData.js
+++ src/components/CarData/carData.js
@@ -36,7 +36,7 @@ const carData = [
         price:'Rs.1300'
     },
     {
-        id : 11,
+        id : 2,
         image: Car11,
         name: 'Koenigsegg',
         fuel: 'Petrol',
@@ -47,7 +47,7 @@ const carData = [
         price:'Rs.13000'
     },
     {
-      id : 1,
+      id : 3,
       image: Car1,
       name: 'Mahindra Thar',
       fuel: 'Petrol',
@@ -58,7 +58,7 @@ const carData = [
       price:'Rs.1500'
   },
     {
-        id : 9,
+        id :4,
         image: Car9,
         name: 'Lamborghini Urus',
         fuel: 'Petrol',
@@ -69,7 +69,7 @@ const carData = [
         price:'Rs.16000'
     },
     {
-        id : 12,
+        id :5,
         image: Car12,
         name: 'Buggati',
         fuel: 'Petrol',
@@ -80,7 +80,7 @@ const carData = [
         price:'Rs.15000'
     },
     {
-        id : 8,
+        id :6,
         image: Car8,
         name: 'Grand i10',
         fuel: 'Petrol',
@@ -91,7 +91,7 @@ const carData = [
         price:'Rs.1400'
     },
     {
-        id : 10,
+        id : 7,
         image: Car10,
         name: 'Rolls Royce',
         fuel: 'Petrol',
@@ -102,7 +102,7 @@ const carData = [
         price:'Rs.20000'
     },
     {
-        id : 10,
+        id : 8,
         image: Car10,
         name: 'Rolls Royce',
         fuel: 'Petrol',
@@ -114,7 +114,7 @@ const carData = [
     },
     
     {
-      id : 11,
+      id : 9,
       image: Car11,
       name: 'Koenigsegg',
       fuel: 'Petrol',
@@ -125,7 +125,7 @@ const carData = [
       price:'Rs.13000'
   },
     {
-        id : 1,
+        id : 10,
         image: Car1,
         name: 'Mahindra Thar',
         fuel: 'Petrol',
@@ -136,7 +136,7 @@ const carData = [
         price:'Rs.1500'
     },
     {
-        id : 3,
+        id : 11,
         image: Car3,
         name: 'Innova ',
         fuel: 'Petrol',
@@ -147,7 +147,7 @@ const carData = [
         price:'Rs.1800'
     },
     {
-        id : 11,
+        id : 12,
         image: Car11,
         name: 'Koenigsegg',
         fuel: 'Petrol',
@@ -158,7 +158,7 @@ const carData = [
         price:'Rs.13000'
     },
     {
-        id : 9,
+        id : 13,
         image: Car9,
         name: 'Lamborghini Urus',
         fuel: 'Petrol',
@@ -169,7 +169,7 @@ const carData = [
         price:'Rs.16000'
     },
     {
-        id : 10,
+        id : 14,
         image: Car10,
         name: 'Rolls Royce',
         fuel: 'Petrol',
@@ -180,7 +180,7 @@ const carData = [
         price:'Rs.20000'
     },
     {
-        id : 2,
+        id : 15,
         image: Car2,
         name: 'Tata Nexon',
         fuel: 'Petrol',
@@ -191,7 +191,7 @@ const carData = [
         price:'Rs.1300'
     },
     {
-        id : 13,
+        id : 16,
         image: Car13,
         name: 'Audi r8',
         fuel: 'Petrol',
@@ -202,7 +202,7 @@ const carData = [
         price:'Rs.15000'
     },
     {
-        id : 4,
+        id : 17,
         image: Car4,
         name: 'Maruti Swift ',
         fuel: 'Petrol',
@@ -213,7 +213,7 @@ const carData = [
         price:'Rs.1100'
     },
     {
-        id : 14,
+        id : 18,
         image: Car14,
         name: 'Bmw i8',
         fuel: 'Petrol',
@@ -224,7 +224,7 @@ const carData = [
         price:'Rs.14000'
     },
     {
-        id : 3,
+        id : 19,
         image: Car3,
         name: 'Innova ',
         fuel: 'Petrol',
@@ -235,7 +235,7 @@ const carData = [
         price:'Rs.1800'
     },
     {
-        id : 10,
+        id : 20,
         image: Car10,
         name: 'Rolls Royce',
         fuel: 'Petrol',
@@ -246,7 +246,7 @@ const carData = [
         price:'Rs.20000'
     },
     {
-        id : 11,
+        id : 21,
         image: Car11,
         name: 'Koenigsegg',
         fuel: 'Petrol',
@@ -257,7 +257,7 @@ const carData = [
         price:'Rs.13000'
     },
     {
-        id : 5,
+        id : 22,
         image: Car5,
         name: 'Honda Amaze ',
         fuel: 'Diesel',
@@ -268,7 +268,7 @@ const carData = [
         price:'Rs.1300'
     },
     {
-        id : 2,
+        id : 23,
         image: Car2,
         name: 'Tata Nexon',
         fuel: 'Petrol',
@@ -279,7 +279,7 @@ const carData = [
         price:'Rs.1300'
     },
     {
-        id : 1,
+        id : 24,
         image: Car1,
         name: 'Mahindra Thar',
         fuel: 'Petrol',
@@ -290,7 +290,7 @@ const carData = [
         price:'Rs.1500'
     },
     {
-        id : 3,
+        id : 25,
         image: Car3,
         name: 'Innova ',
         fuel: 'Petrol',
@@ -301,7 +301,7 @@ const carData = [
         price:'Rs.1800'
     },
     {
-        id : 9,
+        id : 26,
         image: Car9,
         name: 'Lamborghini Urus',
         fuel: 'Petrol',
@@ -312,7 +312,7 @@ const carData = [
         price:'Rs.16000'
     },
     {
-        id : 10,
+        id : 27,
         image: Car10,
         name: 'Rolls Royce',
         fuel: 'Petrol',
@@ -323,7 +323,7 @@ const carData = [
         price:'Rs.20000'
     },
     {
-      id : 2,
+      id : 28,
       image: Car2,
       name: 'Tata Nexon',
       fuel: 'Petrol',
@@ -334,7 +334,7 @@ const carData = [
       price:'Rs.1300'
   },
     {
-        id : 6,
+        id : 29,
         image: Car6,
         name: 'Toyota Innova',
         fuel: 'Diesel',
@@ -345,7 +345,7 @@ const carData = [
         price:'Rs.1800'
     },
     {
-        id : 14,
+        id : 30,
         image: Car14,
         name: 'Bmw i8',
         fuel: 'Petrol',
@@ -356,7 +356,7 @@ const carData = [
         price:'Rs.14000'
     },
     {
-        id : 11,
+        id : 31,
         image: Car11,
         name: 'Koenigsegg',
         fuel: 'Petrol',
@@ -367,7 +367,7 @@ const carData = [
         price:'Rs.13000'
     },
     {
-      id : 1,
+      id : 32,
       image: Car1,
       name: 'Mahindra Thar',
       fuel: 'Petrol',
@@ -378,7 +378,7 @@ const carData = [
       price:'Rs.1500'
   },
     {
-        id : 4,
+        id : 33,
         image: Car4,
         name: 'Maruti Swift ',
         fuel: 'Petrol',
@@ -389,7 +389,7 @@ const carData = [
         price:'Rs.1100'
     },
     {
-      id : 6,
+      id : 34,
       image: Car6,
       name: 'Toyota Innova',
       fuel: 'Diesel',
@@ -400,7 +400,7 @@ const carData = [
       price:'Rs.1800'
   },
   {
-    id : 2,
+    id : 35,
     image: Car2,
     name: 'Tata Nexon',
     fuel: 'Petrol',
@@ -411,7 +411,7 @@ const carData = [
     price:'Rs.1300'
 },
   {
-    id : 9,
+    id : 36,
     image: Car9,
     name: 'Lamborghini Urus',
     fuel: 'Petrol',
@@ -422,7 +422,7 @@ const carData = [
     price:'Rs.16000'
 },
 {
-    id : 14,
+    id : 37,
     image: Car14,
     name: 'Bmw i8',
     fuel: 'Petrol',
@@ -433,7 +433,7 @@ const carData = [
     price:'Rs.14000'
 },
     {
-        id : 5,
+        id : 38,
         image: Car5,
         name: 'Honda Amaze ',
         fuel: 'Diesel',
@@ -608,19 +608,9 @@ const carData = [
       transmission:'Manual',
       price:'Rs.1500'
   },
+  
     {
-        id : 6,
-        image: Car6,
-        name: 'Toyota Innova',
-        fuel: 'Diesel',
-        seats: 8,
-        mileage: 25,
-        year: 2023,
-        transmission:'Manual',
-        price:'Rs.1800'
-    },
-    {
-        id : 3,
+        id : 39,
         image: Car3,
         name: 'Innova ',
         fuel: 'Petrol',
@@ -631,18 +621,18 @@ const carData = [
         price:'Rs.1800'
     },
     {
-        id : 11,
-        image: Car11,
-        name: 'Koenigsegg',
+        id : 40,
+        image: Car1,
+        name: 'Mahindra Thar',
         fuel: 'Petrol',
-        seats: 2,
-        mileage: 10,
-        year: 2021,
+        seats: 5,
+        mileage: 23,
+        year: 2018,
         transmission:'Manual',
-        price:'Rs.13000'
+        price:'Rs.1500'
     },
     {
-        id : 12,
+        id : 41,
         image: Car12,
         name: 'Buggati',
         fuel: 'Petrol',
@@ -653,7 +643,18 @@ const carData = [
         price:'Rs.15000'
     },
     {
-        id : 4,
+        id : 42,
+        image: Car14,
+        name: 'Bmw i8',
+        fuel: 'Petrol',
+        seats: 3,
+        mileage: 13,
+        year: 2018,
+        transmission:'Manual',
+        price:'Rs.14000'
+    },
+    {
+        id : 43,
         image: Car4,
         name: 'Maruti Swift ',
         fuel: 'Petrol',
@@ -664,7 +665,183 @@ const carData = [
         price:'Rs.1100'
     },
     {
-        id : 9,
+        id : 44,
+        image: Car2,
+        name: 'Tata Nexon',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 22,
+        year: 2020,
+        transmission:'Automatic',
+        price:'Rs.1300'
+    },
+    {
+        id : 45,
+        image: Car3,
+        name: 'Innova ',
+        fuel: 'Petrol',
+        seats: 7,
+        mileage: 23,
+        year: 2020,
+        transmission:'Manual',
+        price:'Rs.1800'
+    },
+    {
+        id : 46,
+        image: Car1,
+        name: 'Mahindra Thar',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 23,
+        year: 2018,
+        transmission:'Manual',
+        price:'Rs.1500'
+    },
+    {
+        id : 47,
+        image: Car6,
+        name: 'Toyota Innova',
+        fuel: 'Diesel',
+        seats: 8,
+        mileage: 25,
+        year: 2023,
+        transmission:'Manual',
+        price:'Rs.1800'
+    },
+    {
+        id : 48,
+        image: Car12,
+        name: 'Buggati',
+        fuel: 'Petrol',
+        seats: 2,
+        mileage: 21,
+        year: 2022,
+        transmission:'Manual',
+        price:'Rs.15000'
+    },
+    {
+        id : 49,
+        image: Car14,
+        name: 'Bmw i8',
+        fuel: 'Petrol',
+        seats: 3,
+        mileage: 13,
+        year: 2018,
+        transmission:'Manual',
+        price:'Rs.14000'
+    },
+    {
+        id : 50,
+        image: Car4,
+        name: 'Maruti Swift ',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 28,
+        year: 2023,
+        transmission:'Automatic',
+        price:'Rs.1100'
+    },
+    {
+        id : 51,
+        image: Car9,
+        name: 'Lamborghini Urus',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 12,
+        year: 2022,
+        transmission:'Automatic',
+        price:'Rs.16000'
+    },
+    {
+        id : 52,
+        image: Car1,
+        name: 'Mahindra Thar',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 23,
+        year: 2018,
+        transmission:'Manual',
+        price:'Rs.1500'
+    },
+    {
+        id : 53,
+        image: Car6,
+        name: 'Toyota Innova',
+        fuel: 'Diesel',
+        seats: 8,
+        mileage: 25,
+        year: 2023,
+        transmission:'Manual',
+        price:'Rs.1800'
+    },
+    {
+        id : 54,
+        image: Car3,
+        name: 'Innova ',
+        fuel: 'Petrol',
+        seats: 7,
+        mileage: 23,
+        year: 2019,
+        transmission:'Manual',
+        price : 'Rs.1700'
+    },
+    {
+        id :55,
+        image: Car6,
+        name: 'Toyota Innova',
+        fuel: 'Diesel',
+        seats: 8,
+        mileage: 25,
+        year: 2023,
+        transmission:'Manual',
+        price:'Rs.1800'
+    },
+    {
+        id : 56,
+        image: Car3,
+        name: 'Innova ',
+        fuel: 'Petrol',
+        seats: 7,
+        mileage: 23,
+        year: 2020,
+        transmission:'Manual',
+        price:'Rs.1800'
+    },
+    {
+        id : 57,
+        image: Car11,
+        name: 'Koenigsegg',
+        fuel: 'Petrol',
+        seats: 2,
+        mileage: 10,
+        year: 2021,
+        transmission:'Manual',
+        price:'Rs.13000'
+    },
+    {
+        id : 58,
+        image: Car12,
+        name: 'Buggati',
+        fuel: 'Petrol',
+        seats: 2,
+        mileage: 21,
+        year: 2022,
+        transmission:'Manual',
+        price:'Rs.15000'
+    },
+    {
+        id : 59,
+        image: Car4,
+        name: 'Maruti Swift ',
+        fuel: 'Petrol',
+        seats: 5,
+        mileage: 28,
+        year: 2023,
+        transmission:'Automatic',
+        price:'Rs.1100'
+    },
+    {
+        id : 60,
         image: Car9,
         name: 'Lamborghini Urus',
         fuel: 'Petrol',
@@ -674,5 +851,6 @@ const carData = [
         transmission:'Automatic',
         price:'Rs.16000'
     }
+
   ]
   export default carData
